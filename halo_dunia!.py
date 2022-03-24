83644ef708860380f03f7830b05ff7164d29791e
fanisanur01 <fanisanur07@upi.edu>
2022-03-25 00:54:58 +0700

    revisi lagi

--- halo_dunia!.py
+++ halo_dunia!.py
@@ -7,5 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1ArUCRECN_BpG4HDA6Zmi9WzxDMNIKeIz
 """
 
-print('Halo Dunia !')
-print('Belajar AI !')
+print('Halo Dunia!')
+print('Belajar AI!')
+print('yes!')
